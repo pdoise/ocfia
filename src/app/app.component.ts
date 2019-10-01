89fa4dfb8db08b72cc7a428acc25ab8a9a1f76cb
Phillip Doise <p.doise@gmail.com>
2019-09-30 18:12:47 -0600

    metadata

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,9 +13,12 @@ export class AppComponent {
     title.setTitle('One Call For It All | Colorado Construction Equipment Rental Co.');
 
     meta.addTags([
-      { name: 'brand',   content: 'One Call For It All'},
-      { name: 'location',   content: 'Castle Rock Colorado'},
-      { name: 'keywords', content: 'construction industrial equipment rental, multi quip whisperwatt, indirect fired heater, Generac LED Light Tower, Generac generator'},
+      { name: 'brand',   content: 'One Call For It All, KKM, LTD'},
+      { name: 'location',   content: 'Denver Colorado'},
+      { name: 'keywords', content: 'construction industrial equipment rental, multi quip whisperwatt, indirect fired heater'},
+      { name: 'keywords2', contnet: 'generator rentals, heater rentals, generator sales, heater rentals, ground thaw rentals'},
+      { name: 'keywords3', contnet: 'diesel fuel sales, gold star family owned, home building rentals, indirect fired diesel heaters'},
+      { name: 'keywords4', contnet: 'Generac generator, Generac LED Light Tower, ground thaw sales, diesel fuel delivery'},
       { name: 'description', content: 'equipment rental Denver, Generators, Heaters, Ground Thaw, Light Towers, rentals' }
     ]);
 
